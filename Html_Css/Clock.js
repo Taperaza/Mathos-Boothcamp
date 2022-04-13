7fea2ae3075f6df143ed95895b2d9945cf93ceaa
DESKTOP-LMLVJ79\Larisa <marinb97@live.com>
2022-04-13 21:49:31 +0200

    Adding JavaScript to html.

--- Html_Css/Clock.js
+++ Html_Css/Clock.js
@@ -27,42 +27,25 @@ function showTime() {
 }
 showTime();
 
+var users = [{}]
+document.getElementById('registerButton').onclick = function() {
+  var newUsername = document.getElementById('registerName').value
+  var newPassword = document.getElementById('registerPassword').value
+  users.push({ username: newUsername, password: newPassword })
+}
+document.getElementById('loginButton').onclick = function() {
+  var username = document.getElementById('loginName').value
+  var password = document.getElementById('loginPassword').value
+  for (var i = 0; i < users.length; i++) {
+    if (username == users[i].username) {
+      if (password == users[i].password) {
+        document.getElementById('loginResult').innerHTML = 'Success!'
+        return
+      } else {
+        alert('wrong password')
+      }
+    }
+  }
 
-
-function validate() {
-    var text;
-        if( document.myForm.name.value == "" ) {
-			text = "Name cannot be empty";
-            document.getElementById("demo").innerHTML = text;
-            ocument.myForm.name.focus() ;
-            return false;
-            }
-            if( document.myForm.email.value == "" ) {
-               text = "E-mail cannot be empty";
-               document.getElementById("demo").innerHTML = text;
-               document.myForm.email.focus() ;
-               return false;
-            }
-       var emailID = document.myForm.email.value;
-       atposn = emailID.indexOf("@");
-       dotposn = emailID.lastIndexOf(".");
-       if (atposn < 1 || ( dotposn - atposn < 2 )) {
-			text = "Please enter valid email ID";
-			document.getElementById("demo").innerHTML = text;
-			document.myForm.email.focus() ;
-       return false;
-     }
-            if( document.myForm.phone.value == "" || isNaN( document.myForm.phone.value ) ||
-               document.myForm.phone.value.length != 10 ) {
-               text = "Please enter a valid 10-digit phone number";
-               document.getElementById("demo").innerHTML = text;
-               document.myForm.phone.focus() ;
-               return false;
-            }
-            if( document.myForm.subject.value == "0" ) {
-               text = "Please provide your area of expertise";
-               document.getElementById("demo").innerHTML = text;
-               return false;
-            }
-            return( true );
-         }
+  alert('wrong username')
+}
